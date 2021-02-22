34c59ea62036b3a6cb50c3b18434e789facf922c
GroupOneSEED <79227969+GroupOneSEED@users.noreply.github.com>
2021-02-22 15:22:41 -0700

    Update openCVMiniProject.py

--- openCVMiniProject.py
+++ openCVMiniProject.py
@@ -3,23 +3,19 @@ from picamera import PiCamera as Camera
 import cv2 as cv
 import cv2.aruco as aruco
 import numpy as np
-
+camera = Camera(resolution=(1920, 1080), framerate=60)
 def cameraCalibration():
-    camera = Camera(resolution=(1920, 1080), framerate=60)
     camera.iso = 100
     sleep(2)
     camera.shutter_speed = camera.exposure_speed
     camera.exposure_mode = 'off'
-    awbGains = []
+	gain = camera.awb_gains
+	camera.awb_mode = 'off'
+	camera.awb_gains = gain
     for i in range(3):
         camera.capture('calibration.jpg')
-        awbGains.append(camera.awb_gains)
 
-    AverageGain = (sum(awbGains))/(len(awbGains))
-    print("awb_gains:")
-    print(awbGains)
-    print("Average awb gain value: %f" % AverageGain)
-    return(AverageGain)
+    return(gain)
 
 def detectAruco(awb_gain):
     camera.awb_gains = awb_gains
@@ -32,22 +28,21 @@ def detectAruco(awb_gain):
     cornerList = list(corners)
     bottomRightX = int(cornerList[0][0][2][0]) #need to check this
     bottomLeftX = int(cornerList[0][0][3][0])  #need to check this
-    bottomRightY = int(cornerList[0][0][2][0]) #need to check this
-    bottomLeftY = int(cornerList[0][0][3][0])  #need to check this
+    bottomRightY = int(cornerList[0][0][2][1]) #need to check this
+    bottomLeftY = int(cornerList[0][0][3][1])  #need to check this
     centerX = (bottomRightX + bottomLeftX) / (2)
     centerY = (bottomRightY + bottomLeftY) / (2)
-    if((centerX > 960) and (centerY > 540)):
+    if((centerX > 960) and (centerY < 540)):
         return(0)
-    elif((centerX < 960) and (centerY > 540)):
-        return(1)
     elif((centerX < 960) and (centerY < 540)):
+        return(1)
+    elif((centerX < 960) and (centerY > 540)):
         return(2)
-    elif((centerX > 960) and (centerY < 540)):
+    elif((centerX > 960) and (centerY > 540)):
         return(3)
     else:
         return(4)
 
 gain = cameraCalibration()
-cameraCalibration()
-detectAruco(gain)
+quadrant = detectAruco(gain)
     
