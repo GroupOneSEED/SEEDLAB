7a804d97cbca63789421b2bde6c111b14425cc50
GroupOneSEED <79227969+GroupOneSEED@users.noreply.github.com>
2021-03-21 15:53:27 -0600

    Add files via upload

---
+++ Demo1/Lab2A-Abbr.py
@@ -0,0 +1,29 @@
+print('Enter the tweet abbreviation to decode:')
+print('TWEET> ', end='')
+tweet = input()
+if tweet == 'LOL':
+    print('The decoded tweet abbreviation is:')
+    print('OUTPUT LOL = laughing out loud')
+elif tweet == 'BFN':
+    print('The decoded tweet abbreviation is:')
+    print('OUTPUT BFN = bye for now')
+elif tweet == 'FTW':
+    print('The decoded tweet abbreviation is:')
+    print('OUTPUT FTW = for the win')
+elif tweet == 'IRL':
+    print('The decoded tweet abbreviation is:')
+    print('OUTPUT IRL = in real life')
+elif tweet == 'BTW':
+    print('The decoded tweet abbreviation is:')
+    print('OUTPUT BTW = by the way')
+elif tweet == 'CU':
+    print('The decoded tweet abbreviation is:')
+    print('OUTPUT CU = see you')
+elif tweet == 'AFAIK':
+    print('The decoded tweet abbreviation is:')
+    print('OUTPUT AFAIK = as far as I know')
+elif tweet == 'IDK':
+    print('The decoded tweet abbreviation is:')
+    print('OUTPUT IDK = I don\'t know')
+else:
+    print('Sorry, don\'t know that one!')
